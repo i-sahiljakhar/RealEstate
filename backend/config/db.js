20f57e340f25e79eca0f8beaee70ea9eeceef298
Sahil Chaudhary <Sahiljakhar6030@gmail.com>
2026-08-25 00:18:26 +0530

    RealState project initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,23 @@
-import mongoose, { mongo } from "mongoose";
+// import mongoose, { mongo } from "mongoose";
 
+// export const connectDB = async ()=>{
+//     try{
+
+//         await mongoose.connect('');
+//         console.log('DB CONNECTED')
+//     }
+//     catch(error){
+//         console.error('DB connection error:',error.message)
+//     }
+// }
+
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 export const connectDB = async ()=>{
     try{
-
-        await mongoose.connect('mongodb://localhost:27017/realEstate');
+const MONGO_URI= process.env.MONGO_URI || 'mongodb://localhost:27017/realEstate'
+        await mongoose.connect(MONGO_URI);
         console.log('DB CONNECTED')
     }
     catch(error){
